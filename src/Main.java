0999fd04fc431cb9d2776a3977253264e1be72c8
Senya <frog9615@gmail.com>
2022-01-20 00:33:22 +0300

    update (home)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    static StringBuilder outputStr = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String inputNumber;
@@ -9,18 +10,17 @@ public class Main {
                     "\tВведите номер кредитной карты формата: **** **** **** ****");
             inputNumber = sc.nextLine().trim();
 
-        } while (checkCardNumber(inputNumber));
-//        System.out.println(outputStr);
+        } while (checkCardNumber(inputNumber).length() != 19);
+        System.out.println(outputStr);
 
 //        System.out.println(searchAndReplaceDiamonds("Номер кредитной карты <5484  4864 45684 > 8466", "***"));
 
     }
 
-    public static boolean checkCardNumber(String creditNumber) {
-
+    public static StringBuilder checkCardNumber(String creditNumber) {
 //        int checkFourDigit;
-        StringBuilder outputStr = new StringBuilder();
-        boolean check = false;
+//        StringBuilder outputStr = new StringBuilder();
+//        boolean check = false;
         int index = 0;
         for (int i = 0; i < 4; i++) {
             index = creditNumber.indexOf(' ');
@@ -28,12 +28,10 @@ public class Main {
                if (creditNumber.length() == 4) {
                    index = 4;
                } else {
-                   check = true;
                    break;
                }
             }
         if (index != 4) {
-            check = true;
             break;
         } else {
             try {
@@ -41,12 +39,11 @@ public class Main {
                 outputStr.append(creditNumber.substring(0, index).trim()).append(" ");
                 creditNumber = creditNumber.substring(index).trim();
             } catch (NumberFormatException ignored) {
-                check = true;
                 break;
             }
         }
         }
-        return check;
+        return outputStr;
     }
 
    /* public static String searchAndReplaceDiamonds(String text, String placeholder) {
