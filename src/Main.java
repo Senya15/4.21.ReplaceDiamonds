e7279b57dacaa7bcefa2e47ca445731f89b8f126
Senya <frog9615@gmail.com>
2022-01-20 22:02:08 +0300

    update (home)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-    static StringBuilder outputStr = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String inputNumber;
@@ -9,48 +8,45 @@ public class Main {
             System.out.println("####################\n" +
                     "\tВведите номер кредитной карты формата: **** **** **** ****");
             inputNumber = sc.nextLine().trim();
-
         } while (checkCardNumber(inputNumber).length() != 19);
-        System.out.println(outputStr);
 
-//        System.out.println(searchAndReplaceDiamonds("Номер кредитной карты <5484  4864 45684 > 8466", "***"));
+        System.out.println("Вы ввели номер карты: " + checkCardNumber(inputNumber));
+        System.out.println("\n\tКарта / " +
+                searchAndReplaceDiamonds(checkCardNumber(inputNumber), "***") +
+                " / найдена в базе");
 
     }
 
-    public static StringBuilder checkCardNumber(String creditNumber) {
-//        int checkFourDigit;
-//        StringBuilder outputStr = new StringBuilder();
-//        boolean check = false;
-        int index = 0;
+    public static String checkCardNumber(String creditNumber) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int index;
         for (int i = 0; i < 4; i++) {
             index = creditNumber.indexOf(' ');
             if (i == 3) {
-               if (creditNumber.length() == 4) {
-                   index = 4;
-               } else {
-                   break;
-               }
+                if (creditNumber.length() == 4) {
+                    index = 4;
+                } else {
+                    break;
+                }
             }
-        if (index != 4) {
-            break;
-        } else {
-            try {
-                Integer.parseInt(creditNumber.substring(0, index).trim());
-                outputStr.append(creditNumber.substring(0, index).trim()).append(" ");
-                creditNumber = creditNumber.substring(index).trim();
-            } catch (NumberFormatException ignored) {
+            if (index != 4) {
                 break;
+            } else {
+                try {
+                    Integer.parseInt(creditNumber.substring(0, index).trim());
+                    stringBuilder.append(creditNumber.substring(0, index)).append(" ");
+                    creditNumber = creditNumber.substring(index).trim();
+                } catch (NumberFormatException ignored) {
+                    break;
+                }
             }
         }
-        }
-        return outputStr;
+        return stringBuilder.toString().trim();
     }
 
-   /* public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        int startIndex = text.indexOf('<');
-        int lastIndex = text.lastIndexOf('>');
-        return text.substring(0, startIndex) + placeholder + text.substring(lastIndex +1);
-    }*/
-
+    public static String searchAndReplaceDiamonds(String text, String placeholder) {
+        int index = text.lastIndexOf(' ');
+        return placeholder + " " + text.substring(index + 1);
+    }
 }
 
